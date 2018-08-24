08138d2a5c44b4f7a373ed9e6088c4e798d7833c
Unknown <mairu0514m.o@gmail.com>
2018-08-24 14:24:15 +0900

    listenerページのボタンへの機能付けの準備
    listener.htmlのボタン部分の変更
    views.pyにボタンが押されたときの動作処理を行う関数を追加
    listener_button.pyを作成し、listenerページのボタンが押されたときの関数を定義

--- app/webapp/poll/views.py
+++ app/webapp/poll/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+from . import listener_button
+
 
 def index(request):
     template_name = 'poll/index.html'
@@ -11,6 +13,17 @@ def index(request):
 
 def listener(request):
     template_name = 'poll/listener.html'
+
+    if request.method == 'POST':
+        if 'button_1' in request.POST:
+            # ボタン1がクリックされた場合の処理
+            button1()
+        elif 'button_2' in request.POST:
+            # ボタン2がクリックされた場合の処理
+            button2()
+        elif 'button_3' in request.POST:
+            # ボタン2がクリックされた場合の処理
+            button3()
 
     return render(request, template_name)
 
