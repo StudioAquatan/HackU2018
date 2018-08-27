18259425f8a31b8a2bb69e9ef8a1ff760cdea5d4
Unknown <mairu0514m.o@gmail.com>
2018-08-27 17:31:11 +0900

    listenerページの投票機能の実装

--- app/webapp/poll/listener_button.py
+++ app/webapp/poll/listener_button.py
@@ -2,12 +2,30 @@ from .models import RoomTable, VoteTable, CommentTable
 
 from django.utils import timezone
 
-def button1():
+def set_vote():
+    room = RoomTable.objects.get(id=1)
+    vote = VoteTable(vote_time=timezone.now(), slide_no=0)
+    vote.room_id = room
+    # save the object into the database.
+    return vote
 
+
+def button1():
+    vote = set_vote()
+    vote.vote_type = 1;
+    vote.save()
     return
+
 
 def button2():
+    vote = set_vote()
+    vote.vote_type = 2;
+    vote.save()
     return
 
+
 def button3():
+    vote = set_vote()
+    vote.vote_type = 3;
+    vote.save()
     return
