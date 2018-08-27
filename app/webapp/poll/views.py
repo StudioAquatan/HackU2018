267321b25089f1b606ff818f22f3a14f6108830f
yu <funayama.kit@gmail.com>
2018-08-28 00:23:11 +0900

    表の時刻をグラフと同じ形式に修正

--- app/webapp/poll/views.py
+++ app/webapp/poll/views.py
@@ -99,6 +99,13 @@ def speaker_res(request):
     # コメント
     # 部屋関係なしに全データ
     comments = CommentTable.objects.all().order_by('comment_time')
+    comment_dic_list = []
+    for comment in comments:
+        dic = dict()
+        dic['slide'] = comment.slide_no
+        dic['time'] = re.search(regex, str(timezone.localtime(comment.comment_time))).group()
+        dic['text'] = comment.comment_text
+        comment_dic_list.append(dic)
 
     return render(request, template_name, {
         'slide_list': slide_list,  # スライドのタイトル'slide_n'のリスト，要素数はスライドの枚数
@@ -107,7 +114,7 @@ def speaker_res(request):
         'data1_list': data1_list,
         'data2_list': data2_list,
         'data3_list': data3_list,
-        'comments': comments
+        'comment_dic_list': comment_dic_list
     })
 
 
