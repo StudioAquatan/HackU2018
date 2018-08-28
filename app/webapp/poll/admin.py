2b6ebd5b7b7c5c0804313c02c4e4274c07a3cbe1
Unknown <mairu0514m.o@gmail.com>
2018-08-28 15:03:13 +0900

    DBの変更
    RoomTableからslide_noの削除
    SlideTableの追加
    VoteTableとCommentTableのslide_noをslide_idへ変更

--- app/webapp/poll/admin.py
+++ app/webapp/poll/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RoomTable, VoteTable, CommentTable
+from .models import RoomTable, SlideTable, VoteTable, CommentTable
 
 
 class RoomAdmin(admin.ModelAdmin):
@@ -7,18 +7,23 @@ class RoomAdmin(admin.ModelAdmin):
     list_display_links = ('room_name',)  # 修正リンクでクリックできる項目
 
 
+class SlideAdmin(admin.ModelAdmin):
+    list_display = ('id', 'slide_no', 'start_time', 'end_time', 'room_id',)  # 一覧に出したい項目
+    list_display_links = ('slide_no', 'start_time', 'end_time',)  # 修正リンクでクリックできる項目
+
+
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'vote_type', 'vote_time', 'slide_no', 'room_id',)  # 一覧に出したい項目
+    list_display = ('id', 'vote_type', 'vote_time', 'slide_id', 'room_id',)  # 一覧に出したい項目
     list_display_links = ('vote_type', 'vote_time')  # 修正リンクでクリックできる項目
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'comment_text', 'comment_time', 'slide_no', 'room_id',)  # 一覧に出したい項目
+    list_display = ('id', 'comment_text', 'comment_time', 'slide_id', 'room_id',)  # 一覧に出したい項目
     list_display_links = ('comment_text', 'comment_time')  # 修正リンクでクリックできる項目
 
 
 # RoomTable, VoteTable, CommentTableをadminサイトから触れられるように登録
 admin.site.register(RoomTable, RoomAdmin)
+admin.site.register(SlideTable, SlideAdmin)
 admin.site.register(VoteTable, VoteAdmin)
 admin.site.register(CommentTable, CommentAdmin)
-
