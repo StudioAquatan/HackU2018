9db1416386840e47a30fbc7397c949eed572ff95
Unknown <mairu0514m.o@gmail.com>
2018-08-28 23:54:35 +0900

    ルーム作成に伴うDBの変更
    room_nameをuniqeに変更

--- app/webapp/poll/admin.py
+++ app/webapp/poll/admin.py
@@ -13,12 +13,12 @@ class SlideAdmin(admin.ModelAdmin):
 
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'vote_type', 'vote_time', 'slide_id', 'room_id',)  # 一覧に出したい項目
+    list_display = ('id', 'vote_type', 'vote_time', 'slide_id',)  # 一覧に出したい項目
     list_display_links = ('vote_type', 'vote_time')  # 修正リンクでクリックできる項目
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'comment_text', 'comment_time', 'slide_id', 'room_id',)  # 一覧に出したい項目
+    list_display = ('id', 'comment_text', 'comment_time', 'slide_id',)  # 一覧に出したい項目
     list_display_links = ('comment_text', 'comment_time')  # 修正リンクでクリックできる項目
 
 
--- app/webapp/poll/models.py
+++ app/webapp/poll/models.py
@@ -6,7 +6,7 @@ class RoomTable(models.Model):
     ルームのモデル
     """
     # ルーム名
-    room_name = models.CharField(max_length=32)
+    room_name = models.CharField(max_length=32, unique=True)
     # パスワード
     password = models.CharField(max_length=32)
     # リスナーの人数
@@ -40,8 +40,6 @@ class VoteTable(models.Model):
     vote_time = models.DateTimeField('date published')
     # 投票時のスライドID
     slide_id = models.ForeignKey(SlideTable, on_delete=models.CASCADE)
-    # ルームID
-    room_id = models.ForeignKey(RoomTable, on_delete=models.CASCADE)
 
 
 class CommentTable(models.Model):
@@ -54,5 +52,3 @@ class CommentTable(models.Model):
     comment_time = models.DateTimeField('date published')
     # コメント送信時のスライドID
     slide_id = models.ForeignKey(SlideTable, on_delete=models.CASCADE)
-    # ルームID
-    room_id = models.ForeignKey(RoomTable, on_delete=models.CASCADE)
