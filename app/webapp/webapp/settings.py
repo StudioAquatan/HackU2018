b84bd2d0b4164d3f4a07f1618f812afae64cee21
shanpu <cheztomo513@gmail.com>
2018-08-30 21:24:59 +0900

    CORSに対応

--- app/webapp/webapp/settings.py
+++ app/webapp/webapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'bootstrap4',
     'django_filters',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -53,9 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'webapp.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
