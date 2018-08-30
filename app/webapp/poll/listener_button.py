b207799f5d33d53443c3ec077c2e8656555775c8
Unknown <mairu0514m.o@gmail.com>
2018-08-31 04:36:58 +0900

    roomの状態によってボタンの反応と画面の表示を変更

--- app/webapp/poll/listener_button.py
+++ app/webapp/poll/listener_button.py
@@ -9,58 +9,62 @@ def get_slide(get_id):
     # room情報からslideレコードを取得
     if SlideTable.objects.filter(room_id=room).exists():
         slide = SlideTable.objects.filter(room_id=room).latest('start_time')
-        return slide
+        if slide.end_time is None:
+            return slide
+        else:
+            # 最新のスライドが終了していれば講義が終了していると判断して-2を返す
+            return -2
     else:
-        # slideがなければFalseを返す
-        return False
+        # slideがなければ-2を返す
+        return -1
 
 
 def button1(room_id):
     slide = get_slide(room_id)
-    # slideがFalseでなければ
-    if slide:
+    if slide != -1 and slide != -2:
+        # slideが-1または-2でなければ
         # slide情報からvoteを作成
         vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
         vote.vote_type = 1
         vote.save()
-        return True
+        return 1
     else:
-        return False
+        return slide
 
 
 def button2(room_id):
     slide = get_slide(room_id)
-    # slideがFalseでなければ
-    if slide:
+    if slide != -1 and slide != -2:
+        # slideが-1または-2でなければ
         # slide情報からvoteを作成
         vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
         vote.vote_type = 2
         vote.save()
-        return True
+        return 1
     else:
-        return False
+        return slide
 
 
 def button3(room_id):
     slide = get_slide(room_id)
-    # slideがFalseでなければ
-    if slide:
+    if slide != -1 and slide != -2:
+        # slideが-1または-2でなければ
         # slide情報からvoteを作成
         vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
         vote.vote_type = 3
         vote.save()
-        return True
+        return 1
     else:
-        return False
+        return slide
 
 
 def comment_submit(input_comment, room_id):
     slide = get_slide(room_id)
-    # slideがFalseでなければ
-    if slide:
+    if slide != -1 and slide != -2:
+        # slideが-1または-2でなければ
         # slide情報からcommentを作成
         comment = CommentTable(comment_text=input_comment, comment_time=timezone.now(), slide_id=slide)
         comment.save()
-        return True
+        return 2
     else:
-        return False
+        return slide
