36896f682297dd031f0c862af15996e686dbf2da
Unknown <mairu0514m.o@gmail.com>
2018-08-30 22:34:02 +0900

    /listener_comment: room_id追加による修正

--- app/webapp/poll/listener_button.py
+++ app/webapp/poll/listener_button.py
@@ -4,37 +4,40 @@ from django.utils import timezone
 from django.db import models
 
 
-def set_vote():
-    room = RoomTable.objects.get(id=1)
-    # room情報からslide情報を取得
+def get_slide(get_id):
+    # room_idからroomレコードを取得
+    room = RoomTable.objects.get(id=get_id)
+    # room情報からslideレコードを取得
     slide = SlideTable.objects.filter(room_id=room).latest('start_time')
+    return slide
+
+
+def button1(room_id):
+    slide = get_slide(room_id)
     # slide情報からvoteを作成
     vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
-    return vote
-
-
-def button1():
-    vote = set_vote()
     vote.vote_type = 1
     vote.save()
 
 
-def button2():
-    vote = set_vote()
+def button2(room_id):
+    slide = get_slide(room_id)
+    # slide情報からvoteを作成
+    vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
     vote.vote_type = 2
     vote.save()
 
 
-def button3():
-    vote = set_vote()
+def button3(room_id):
+    slide = get_slide(room_id)
+    # slide情報からvoteを作成
+    vote = VoteTable(vote_time=timezone.now(), slide_id=slide)
     vote.vote_type = 3
     vote.save()
 
 
-def comment_submit(input_comment):
-    room = RoomTable.objects.get(id=1)
-    # room情報からslide情報を取得
-    slide = SlideTable.objects.filter(room_id=room).latest('start_time')
+def comment_submit(input_comment, room_id):
+    slide = get_slide(room_id)
     # slide情報からcommentを作成
     comment = CommentTable(comment_text=input_comment, comment_time=timezone.now(), slide_id=slide)
     comment.save()
