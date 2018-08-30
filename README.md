696468d75d93d7481a6a876485c41d531b928c5b
shanpu <30888767+chez-shanpu@users.noreply.github.com>
2018-08-31 02:40:55 +0900

    Revert "講義開始前にvoteとcommentをしようとすると警告が出るように変更"

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# poll(仮)
-## 動作環境
-python 3.6.4
-django 2.1
-django-bootstrap4  0.0.6
+HackU2018
--- app/webapp/poll/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import RoomTable
-
-
-class RoomForm(ModelForm):
-    """ルームのフォーム"""
-    class Meta:
-        model = RoomTable
-        fields = ('room_name',)
